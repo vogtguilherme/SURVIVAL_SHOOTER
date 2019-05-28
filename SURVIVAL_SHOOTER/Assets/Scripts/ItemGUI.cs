82968082ea4749df39914152ac890f672f086d6e
brklipel <30844487+brklipel@users.noreply.github.com>
2019-05-28 00:14:49 -0300

    Lojinha 0.1

---
+++ SURVIVAL_SHOOTER/Assets/Scripts/ItemGUI.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemGUI : MonoBehaviour
+{
+    public Image image;
+    public Text nameText, costText, descriptionText, buttonBuyText;
+
+    public Button buttonBuy;
+
+    int id;
+
+    public void SetItem(int p_id, string p_name, string p_description, int p_cost, Sprite p_image, bool p_bought)
+    {
+        id = p_id;
+
+        nameText.text = p_name;
+        descriptionText.text = p_description;
+
+        costText.text = "$" + p_cost;
+
+        image.sprite = p_image;
+
+        if (p_bought)
+        {
+            buttonBuyText.text = p_bought ? "Bought" : "Buy";
+
+            buttonBuy.interactable = !p_bought;
+        }
+        else
+        {
+            buttonBuyText.text = PlayerManager.instance.Money >= p_cost ? "Buy" : "No enough money";
+
+            buttonBuy.interactable = PlayerManager.instance.Money >= p_cost ? true : false;
+        }
+    }
+
+    private void Update()
+    {
+        if(gameObject.activeInHierarchy)
+        {
+            if (!PlayerManager.instance.PlayerItems[id].bought)
+            {
+                if (buttonBuy.interactable && PlayerManager.instance.Money < PlayerManager.instance.PlayerItems[id].cost)
+                {
+                    buttonBuy.interactable = false;
+                    buttonBuyText.text = "No enough money";
+                }
+                else if(!buttonBuy.interactable && PlayerManager.instance.Money >= PlayerManager.instance.PlayerItems[id].cost)
+                {
+                    buttonBuy.interactable = true;
+                    buttonBuyText.text = "Buy";
+                }
+            }
+        }
+    }
+
+    public void BuyItem()
+    {
+        PlayerManager.instance.BuyItem(id);
+    }
+}
---
+++ SURVIVAL_SHOOTER/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemPlayer
+{
+    public string itemName;
+    public string description;
+    public int cost;
+    public bool bought;
+    public Sprite image;
+
+    public itemPlayer(Item p_item)
+    {
+        itemName = p_item.itemName;
+        description = p_item.description;
+        cost = p_item.cost;
+        bought = p_item.bought;
+        image = p_item.image;
+    }
+}
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager instance;
+
+    [SerializeField]
+    int money = 100;
+    public int Money { get { return money; } }
+
+    public List<Item> itemsResources = new List<Item>();
+
+    List<itemPlayer> playerItems = new List<itemPlayer>();
+    public List<itemPlayer> PlayerItems { get { return playerItems; } }
+
+    private void Awake()
+    {
+        instance = this;
+
+        for(int i = 0; i < itemsResources.Count; i++)
+        {
+            playerItems.Add(new itemPlayer(itemsResources[i]));
+        }
+    }
+
+
+    public void BuyItem(int p_id)
+    {
+        money -= playerItems[p_id].cost;
+        playerItems[p_id].bought = true;
+
+        ShopGUI.instance.FillShop();
+    }
+}
