605be238f5c2dc18c2d10bed12e9f3f2fd623eb2
Guilherme <vogt.guilherme@gmail.com>
2019-05-28 20:26:30 -0300

    Screen Fader e Scene Controller
    LEMBRETE: ALFANUMÉRICO 9 E 0 FAZEM FADE

---
+++ SURVIVAL_SHOOTER/Assets/Scripts/MonoBehaviors/SceneController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public event Action OnSceneUnload;
+    public event Action OnSceneLoaded;
+
+    private ScreenFader m_ScreenFader;
+
+    private void Awake()
+    {
+        m_ScreenFader = GetComponent<ScreenFader>();
+    }
+
+    public void FadeAndLoadScene()
+    {
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            m_ScreenFader.FadeIn();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            m_ScreenFader.FadeOut();
+        }
+    }
+
+    private IEnumerator FadeAndSwitchScenes(int sceneBuildIndex)
+    {
+        yield return null;
+    }
+
+    private IEnumerator LoadSceneAndSetActive(int sceneBuildIndex)
+    {
+        yield return null;
+    }
+}
---
+++ SURVIVAL_SHOOTER/Assets/Scripts/MonoBehaviors/UI/ScreenFader.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScreenFader : MonoBehaviour
+{
+    public event Action OnFadeCompleted;
+
+    [SerializeField] private Image m_FadeImage;
+    [SerializeField] private Color m_FadeColor;
+    [SerializeField] private float m_FadeDuration;
+    public bool FadeOnSceneLoad;
+
+    private Color m_FadedOutColor;
+    private bool isFading = false;
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += HandleSceneLoaded;
+
+        m_FadedOutColor = new Color(m_FadeColor.r, m_FadeColor.g, m_FadeColor.b, 0);
+    }
+
+    private void HandleSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (FadeOnSceneLoad)
+            FadeIn();
+    }
+
+    private void Start()
+    {
+        if (FadeOnSceneLoad)
+            FadeIn();
+    }
+
+    public void FadeIn()
+    {
+        if (isFading)
+            return;
+
+        StartCoroutine("StartFadeIn");
+    }
+
+    public void FadeOut()
+    {
+        if (isFading)
+            return;
+
+        StartCoroutine("StartFadeOut");
+    }
+
+    private IEnumerator StartFadeIn()
+    {
+        yield return StartFade(m_FadeColor, m_FadedOutColor, m_FadeDuration);
+    }
+
+    private IEnumerator StartFadeOut()
+    {
+        yield return StartFade(m_FadedOutColor, m_FadeColor, m_FadeDuration);
+    }
+
+    private IEnumerator StartFade(Color from, Color to, float duration)
+    {       
+        isFading = true;
+
+        float startTime = 0f;
+
+        while (startTime <= duration)
+        {
+            m_FadeImage.color = Color.Lerp(from, to, startTime / duration);
+
+            startTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        isFading = false;
+
+        Debug.Log("Fade Completed");
+
+        if (OnFadeCompleted != null)
+        {
+            OnFadeCompleted();
+            OnFadeCompleted = null;
+        }
+    }
+}
