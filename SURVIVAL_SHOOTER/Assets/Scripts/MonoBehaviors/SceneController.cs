91efe3c5b0d26b243db719400f118ba6cae7c314
Guilherme <vogt.guilherme@gmail.com>
2019-05-28 21:13:39 -0300

    Scene Manager
    Fazendo fade e trocando de cenas de maneira aditiva (PersistentScene permance carregada)

--- SURVIVAL_SHOOTER/Assets/Scripts/MonoBehaviors/SceneController.cs
+++ SURVIVAL_SHOOTER/Assets/Scripts/MonoBehaviors/SceneController.cs
@@ -10,16 +10,31 @@ public class SceneController : MonoBehaviour
     public event Action OnSceneUnload;
     public event Action OnSceneLoaded;
 
-    private ScreenFader m_ScreenFader;
+    [SerializeField]private bool FadeOnSceneLoad;
+
+    public string sceneToLoad;
+
+    private ScreenFader m_ScreenFader;    
 
     private void Awake()
     {
         m_ScreenFader = GetComponent<ScreenFader>();
+    }    
+
+    private IEnumerator Start()
+    {
+        yield return StartCoroutine(LoadSceneAndSetActive(sceneToLoad));
+
+        if (FadeOnSceneLoad)
+            m_ScreenFader.FadeIn();
     }
 
-    public void FadeAndLoadScene()
+    public void FadeAndLoadScene(string sceneName)
     {
-
+        if(!m_ScreenFader.IsFading)
+        {
+            StartCoroutine(FadeAndSwitchScenes(sceneName));
+        }
     }
 
     private void Update()
@@ -33,15 +48,53 @@ public class SceneController : MonoBehaviour
         {
             m_ScreenFader.FadeOut();
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            FadeAndLoadScene("Stage01");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            FadeAndLoadScene("Stage02");
+        }
     }
 
-    private IEnumerator FadeAndSwitchScenes(int sceneBuildIndex)
+    private IEnumerator FadeAndSwitchScenes(string sceneName)
     {
-        yield return null;
+        m_ScreenFader.FadeOut();
+
+        while(m_ScreenFader.IsFading)
+        {
+            Debug.Log("Fading...");
+
+            yield return null;
+        }
+
+        OnSceneUnload?.Invoke();
+
+        yield return SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+
+        yield return StartCoroutine(LoadSceneAndSetActive(sceneName));
+
+        OnSceneLoaded?.Invoke();
+
+        m_ScreenFader.FadeIn();
+
+        while (m_ScreenFader.IsFading)
+        {
+            Debug.Log("Fading...");
+
+            yield return null;
+        }
     }
 
-    private IEnumerator LoadSceneAndSetActive(int sceneBuildIndex)
+    private IEnumerator LoadSceneAndSetActive(string sceneName)
     {
-        yield return null;
+        yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        Scene newLoadedScene = SceneManager.GetSceneAt(SceneManager.sceneCount - 1);
+
+        SceneManager.SetActiveScene(newLoadedScene);
     }
 }
--- SURVIVAL_SHOOTER/Assets/Scripts/MonoBehaviors/UI/ScreenFader.cs
+++ SURVIVAL_SHOOTER/Assets/Scripts/MonoBehaviors/UI/ScreenFader.cs
@@ -10,30 +10,20 @@ public class ScreenFader : MonoBehaviour
 
     [SerializeField] private Image m_FadeImage;
     [SerializeField] private Color m_FadeColor;
-    [SerializeField] private float m_FadeDuration;
-    public bool FadeOnSceneLoad;
+    [SerializeField] private float m_FadeDuration;    
 
     private Color m_FadedOutColor;
     private bool isFading = false;
 
+    public bool IsFading
+    {
+        get { return isFading; }
+    }
+
     private void Awake()
     {
-        SceneManager.sceneLoaded += HandleSceneLoaded;
-
         m_FadedOutColor = new Color(m_FadeColor.r, m_FadeColor.g, m_FadeColor.b, 0);
-    }
-
-    private void HandleSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if (FadeOnSceneLoad)
-            FadeIn();
-    }
-
-    private void Start()
-    {
-        if (FadeOnSceneLoad)
-            FadeIn();
-    }
+    }    
 
     public void FadeIn()
     {
